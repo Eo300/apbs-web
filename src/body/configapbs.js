ddb1939cf943a1cdd97efa0725f58bd3d7737043
Offor <elvis.offor@pnnl.gov>
2019-04-05 17:34:49 -0700

    APBS form building begins. Incomplete for now

--- src/body/configapbs.js
+++ src/body/configapbs.js
@@ -3,8 +3,11 @@ import 'antd/dist/antd.css'
 import  { Affix, Layout, Menu, Button, Form, Switch,
           Input, Radio, Checkbox , Row, Col, InputNumber,
           Icon, Tooltip, Upload,
+          Collapse
         } from 'antd';
+import RadioGroup from 'antd/lib/radio/group';
 const { Content, Sider } = Layout;
+const Panel = Collapse.Panel;
 
 /**
  * Component defining how the APBS job configuration page is rendered
@@ -27,6 +30,44 @@ class ConfigAPBS extends Component{
         </Sider>
       </Affix>
     )
+  }
+
+  /** 
+   * renderDropdown(panelHeader =string, options =array)
+   *    panelHeader:  string to display on the header field of Panel component
+   *    options:      array collection of form objects to display within Panel
+   */
+  renderCollapsePanel(panelHeader, optionObjs){
+    // for (option of optionObjs){
+
+    // }
+    return(
+      <Panel header={panelHeader} forceRender={true}>
+        {optionObjs}
+      </Panel>
+    );
+  }
+
+  renderOutputFormat(){
+    let header = 'FORMAT TO WRITE DATA:';
+    let outputNameField = 'writeformat';
+    let radioOptions = [];
+    let outputOptions = 
+      { 'dx'  : 'OpenDX', 
+        'avs' : 'AVS UCD',
+        'uhbd': 'UBHD'
+      };
+
+    for (let optVal in outputOptions){
+      radioOptions.push(
+        <Radio value={optVal}> {outputOptions[optVal]} </Radio>
+      )
+    }
+    let outputGroup = 
+      <RadioGroup name={outputNameField}> {radioOptions} </RadioGroup>
+    ;
+
+    return this.renderCollapsePanel(header, outputGroup);
   }
 
   /** Creates and returns the APBS configuration form.
@@ -52,7 +93,18 @@ class ConfigAPBS extends Component{
 
     return(
       <Form action="/jobstatus?submitType=apbs" method="POST" onSubmit={this.handleJobSubmit} name="thisform" enctype="multipart/form-data">
-        
+        {/** Choose the calculation type */}
+        <Form.Item label='Calculation Type'>
+        </Form.Item>
+
+
+        {/** Choose format of the data output */}
+        <Form.Item label='Output'>
+          <Collapse>
+            {this.renderOutputFormat()}
+          </Collapse>
+        </Form.Item>
+
         {/** Where the submission button lies */}
         <Form.Item>
           <Button type="primary" htmlType="submit">Start Job</Button>
@@ -63,8 +115,8 @@ class ConfigAPBS extends Component{
       
   render(){
     return(
-      <Layout id="pdb2pqr" style={{ padding: '16px 0', marginBottom: 5, background: '#fff', boxShadow: "2px 4px 10px #00000033" }}>
-          {this.renderSidebar()}
+      <Layout id="apbs" style={{ padding: '16px 0', marginBottom: 5, background: '#fff', boxShadow: "2px 4px 10px #00000033" }}>
+          {/* {this.renderSidebar()} */}
           <Layout>
             <Content style={{ background: '#fff', padding: 16, margin: 0, minHeight: 280 }}>
               {/* Content goes here */}
